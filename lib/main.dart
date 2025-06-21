493b385d842f0934a48910e729bc303d99b96136
Miyahara Yuuki <59301668+raikiri-jp@users.noreply.github.com>
2025-06-21 21:45:06 +0900

    IsarService の初期化

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:event_manager/services/isar_service.dart';
 import 'package:flutter/material.dart';
 import 'package:event_manager/app/app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await IsarService().init();
   runApp(const EventManagerApp());
 }
