4ea3e71487b4988581d9eecbcb42b9be4492cd5b
Miyahara Yuuki <59301668+raikiri-jp@users.noreply.github.com>
2025-06-21 21:40:32 +0900

    EventServiceに日付範囲でイベント取得メソッドを追加

--- lib/features/calendar/widgets/custom_calendar.dart
+++ lib/features/calendar/widgets/custom_calendar.dart
@@ -1,14 +1,58 @@
 import 'package:flutter/material.dart';
 import '../../../models/event.dart';
+import '../../../services/event_service.dart';
 import 'calendar_cell.dart';
 
-class CustomCalendar extends StatelessWidget {
+class CustomCalendar extends StatefulWidget {
   final DateTime focusedMonth;
 
   const CustomCalendar({
     super.key,
     required this.focusedMonth,
   });
+
+  @override
+  State<CustomCalendar> createState() => _CustomCalendarState();
+}
+
+class _CustomCalendarState extends State<CustomCalendar> {
+  final EventService _eventService = EventService();
+  Map<DateTime, List<Event>> _eventsMap = {};
+
+  @override
+  void didUpdateWidget(covariant CustomCalendar oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.focusedMonth != widget.focusedMonth) {
+      _loadEvents();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadEvents();
+  }
+
+  void _loadEvents() async {
+    final start = DateTime(widget.focusedMonth.year, widget.focusedMonth.month, 1);
+    final end = DateTime(widget.focusedMonth.year, widget.focusedMonth.month + 1, 0, 23, 59, 59);
+
+    final events = await _eventService.getEventsBetween(start, end);
+
+    // 日付ごとにまとめる
+    final Map<DateTime, List<Event>> map = {};
+    for (var event in events) {
+      final date = DateTime(event.start.year, event.start.month, event.start.day);
+      if (!map.containsKey(date)) {
+        map[date] = [];
+      }
+      map[date]!.add(event);
+    }
+
+    setState(() {
+      _eventsMap = map;
+    });
+  }
 
   DateTime _firstDayOfCalendar(DateTime month) {
     final firstDay = DateTime(month.year, month.month, 1);
@@ -17,7 +61,7 @@ class CustomCalendar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final start = _firstDayOfCalendar(focusedMonth);
+    final start = _firstDayOfCalendar(widget.focusedMonth);
     final days = List.generate(42, (i) => start.add(Duration(days: i)));
 
     return Column(
@@ -32,8 +76,7 @@ class CustomCalendar extends StatelessWidget {
             ),
             itemBuilder: (context, index) {
               final date = days[index];
-              // TODO: イベント取得処理
-              final List<Event> events = [];
+              final events = _eventsMap[DateTime(date.year, date.month, date.day)] ?? [];
               return CalendarCell(date: date, events: events);
             },
           ),
