5c7bfd8d88841a4ccec834b20e8f6d4a5275df9f
Miyahara Yuuki <59301668+raikiri-jp@users.noreply.github.com>
2025-06-29 00:30:26 +0900

    Fix IsarService

--- lib/services/isar_service.dart
+++ lib/services/isar_service.dart
@@ -3,34 +3,30 @@ import 'package:path_provider/path_provider.dart';
 import '../models/event.dart';
 
 class IsarService {
-  late Future<Isar> db;
+  static Isar? _isar;
 
-  IsarService() {
-    db = _initIsar();
-  }
-
-  Future<Isar> _initIsar() async {
-    final dir = await getApplicationDocumentsDirectory();
-    return await Isar.open(
-      [EventSchema],
-      directory: dir.path,
-    );
+  static Future<Isar> getInstance() async {
+    if (_isar == null) {
+      final dir = await getApplicationDocumentsDirectory();
+      _isar = await Isar.open([EventSchema], directory: dir.path);
+    }
+    return _isar!;
   }
 
   Future<void> addEvent(Event event) async {
-    final isar = await db;
+    final isar = await getInstance();
     event.createdAt = DateTime.now();
     event.updatedAt = DateTime.now();
     await isar.writeTxn(() => isar.events.put(event));
   }
 
   Future<List<Event>> getAllEvents() async {
-    final isar = await db;
+    final isar = await getInstance();
     return await isar.events.where().findAll();
   }
 
   Future<void> deleteEvent(int id) async {
-    final isar = await db;
+    final isar = await getInstance();
     await isar.writeTxn(() => isar.events.delete(id));
   }
 }
