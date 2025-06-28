4022d78f851d48edbdbea1d4f9498ec48b6a950f
Miyahara Yuuki <59301668+raikiri-jp@users.noreply.github.com>
2025-06-29 00:06:26 +0900

    イベント追加ボタンを追加

--- lib/features/calendar/calendar_screen.dart
+++ lib/features/calendar/calendar_screen.dart
@@ -1,19 +1,58 @@
+import 'package:event_manager/features/calendar/event_list_screen.dart';
 import 'package:event_manager/features/calendar/widgets/custom_calendar.dart';
+import 'package:event_manager/features/event/event_form_screen.dart';
 import 'package:flutter/material.dart';
 
-class CalendarScreen extends StatelessWidget {
+class CalendarScreen extends StatefulWidget {
   const CalendarScreen({super.key});
+
+  @override
+  State<CalendarScreen> createState() => _CalendarScreenState();
+}
+
+class _CalendarScreenState extends State<CalendarScreen> {
+  bool _isNavigating = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Event Manager')),
+      appBar: AppBar(title: const Text('カレンダー')),
       body: CustomCalendar(
         initialMonth: DateTime.now(),
-        onDateTap: (selectedDate) {
-          // ここで詳細画面に遷移など
-        },
+        onDateTap: _handleDateTap,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _handleAddEvent,
+        tooltip: 'イベントを追加',
+        child: Icon(Icons.add),
       ),
     );
+  }
+
+  void _handleDateTap(DateTime selectedDate) async {
+    if (_isNavigating) return;
+    _isNavigating = true;
+
+    await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (_) => EventListScreen(selectedDate: selectedDate),
+      ),
+    );
+    _isNavigating = false;
+  }
+
+  void _handleAddEvent() async {
+    if (_isNavigating) return;
+    _isNavigating = true;
+
+    await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (_) => const EventFormScreen(), // イベント作成画面
+      ),
+    );
+    // TODO: DB変更があれば setState() などで再描画
+    _isNavigating = false;
   }
 }
